a34a5cd30c7e460446ce90c3f6a983c6c165ea79
Mikel López <39565650+mlvillarroya@users.noreply.github.com>
2021-12-04 08:24:43 +0100

    day4 begins

---
+++ Day4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(Directory.GetParent(Environment.CurrentDirectory)?.Parent.Parent +
+                                          "/files/input.txt");
+        }
+    }
+}
